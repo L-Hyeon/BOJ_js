1a684ff90ec5db9ca426efe6ec2c7e76ad65a641
L-Hyun <dkh1001@outlook.kr>
2022-07-18 13:02:25 +0900

    x

---
+++ #단계별/1. 입출력과 사칙연산/10998_A x B.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const inputData = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+let A = parseInt(inputData[0]);
+let B = parseInt(inputData[1]);
+
+console.log(A * B);
